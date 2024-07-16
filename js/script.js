1e46e83f4bd3e31ff4523f4a897e27235b553d5d
NeyleCM <neylecmorantes@gmail.com>
2024-07-16 23:46:51 +0200

    fecha-hora- tiempo-contraseña

--- js/script.js
+++ js/script.js
@@ -67,3 +67,77 @@ const generarPassword = () => {
         }
 
         buttonPassword.addEventListener('click', generarPassword)
+
+        // ---- CLIMA -----
+
+const containerTime = document.getElementById('container-Time')
+
+        let city = 'Madrid'
+        const apiKey = '6eed3220b1cc4ec29ba192021241607'
+        const url = `https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${city}&aqi=no`
+
+        const getTime = async () => {
+            try {
+            const response = await fetch(url)
+            const data = await response.json()
+            return data
+        } catch(error) {
+         console.log('Este es el error', error)
+    }
+}
+
+getTime().then(city => {
+    const {current, forecast, location} = city
+console.log(city)
+const dataHour = forecast.forecastday[0].hour
+const hoursData = dataHour.map(hour => {
+    return `<div class="time-Hour">${hour.time} <img src=${current.condition.icon} alt="icon"/> ${hour.temp_c}°C </div>`;
+}).join('');
+    containerTime.innerHTML = `<p>${location.name} / ${location.country} </p> <p>${current.condition.text} </p> 
+    <img src=${current.condition.icon} alt="icon" class="iconBig"/> </br>
+    ${current.precip_in}% </br>
+    ${current.humidity}% </br>
+    ${current.wind_kph}Km/h
+    <div class="time-Hours">${hoursData}</div>
+    `
+})
+
+
+//current.condition.icon / text "Sunny" / wind_kph
+//forecast . forecastday .0 . hour
+//location . country . name ó region
+
+        // AÑADE TU ENLACE
+
+        const nameEnlace = document.getElementById('input-nameEnlace').value
+        const inputEnlace = document.getElementById('input-enlace').value
+        const buttonEnlaces = document.getElementById('buttonEnlaces')
+        const ulList = document.getElementById('ul-list')
+
+        const saveEnlaces = () => {
+            let object = {nameEnlace, inputEnlace}
+            const objetoEnlace = localStorage.setItem('nameEnlace', 'inputEnlace') // en la primera vuelta no existe, en la segunda si.
+            
+            const name = object ? JSON.parse(object) : [] // nos queda un array vacio en la primera.
+        
+            name.push() // aqui el array tiene el chiste
+        
+            // console.log(array); //deberia mostrar chiste
+        
+            localStorage.setItem('name', JSON.stringify(name))
+            
+            template(localStorage.getItem('nameEnlace', 'inputEnlace'))//Invocamos esta funcion y le pasamos los jokes guardados en la variable localStorage
+        }
+
+
+/*
+        const formulario = document.getElementById('formulario')
+        console.log(formulario)
+
+        const extraerEnlaces = () => {
+            console.log('Validando...')
+        }
+
+        formulario.addEventListener('submit', extraerEnlaces)
+
+    */
